5e54ae39d52752c617d67d2ee305bba6facb50b7
emanuelegurini <emanuele.gurini89@gmail.com>
2023-01-03 22:35:27 +0100

    Add: index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,119 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+import { Suspense } from "react";
+import Container from "../components/Container";
+import VideoCard from "../components/VideoCard";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Suspense fallback={null}>
+      <Container>
+        <div className="flex flex-col justify-center items-start max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pb-16">
+          <div className="flex flex-col-reverse sm:flex-row items-start">
+            <div className="flex flex-col pr-8">
+              <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white">
+                Emanuele Gurini
+              </h1>
+              <h2 className="text-gray-700 dark:text-gray-200 mb-4">
+                Software Developer at{" "}
+                <span className="font-semibold">Flowing/Claranet</span>
+              </h2>
+              <p className="text-gray-600 dark:text-gray-400 mb-16">
+                Supporting developers with the tools and knowledge to build fast
+                and reliable web applications through a collaborative and
+                supportive learning experience. Featuring courses on web
+                development, serverless design, and React/Next.js
+              </p>
+            </div>
+            <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto">
+              <Image
+                alt="Emanuele Gurini"
+                height={176}
+                width={176}
+                src="/avatar.jpg"
+                sizes="30vw"
+                priority
+                className="rounded-full filter grayscale"
+              />
+            </div>
+          </div>
+
+          <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-6 text-black dark:text-white">
+            Featured Posts
+          </h3>
+          <Link
+            href="/blog"
+            className="flex items-center mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6"
+          >
+            <>
+              {"Read all posts"}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="h-6 w-6 ml-1"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
+                />
+              </svg>
+            </>
+          </Link>
+
+          <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-16 text-black dark:text-white">
+            Video Section
+          </h3>
+          <p className="text-gray-600 dark:text-gray-400 mb-4">
+            This section provides in-depth videos on front-end and low level
+            programming. Dive into the technical details of front-end
+            development, including HTML, CSS, and JavaScript, as well as modern
+            frameworks and libraries
+          </p>
+          <VideoCard
+            index="01"
+            href="https://www.youtube.com/watch?v=Jj20Bxpdnls&t=101s"
+            length="02:46"
+            title="Tailwind CSS - Specifying which files to process"
+          />
+          <VideoCard
+            index="02"
+            href="https://www.youtube.com/watch?v=AGl52moyISU&list=PL6bwFJ82M6FXgctyoWXqj7H0GK8_YIeF1&index=3"
+            length="54:22"
+            title="The Dependency Hell in Node.js: How to Manage Dependency Conflicts"
+          />
+
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.youtube.com/playlist?list=PL6bwFJ82M6FXgctyoWXqj7H0GK8_YIeF1"
+            className="flex items-center mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6"
+          >
+            Watch all videos
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="h-6 w-6 ml-1"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
+              />
+            </svg>
+          </a>
+          <span className="h-16" />
+        </div>
+      </Container>
+    </Suspense>
+  );
 };
 
 export default Home;
