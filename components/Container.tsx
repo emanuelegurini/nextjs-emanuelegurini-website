ce9af5dc893f8058ad9e5cddb93b576eb6710f64
emanuelegurini <emanuele.gurini89@gmail.com>
2023-01-03 22:34:34 +0100

    Add: layout, footer and header with nav (desktop and mobile)

---
+++ components/Container.tsx
@@ -0,0 +1,80 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import NextLink from "next/link";
+import cn from "classnames";
+
+import Footer from "./Footer";
+import MobileMenu from "./MobileMenu";
+//import MobileMenu from "components/MobileMenu";
+
+function NavItem({ href, text }: any) {
+  const router = useRouter();
+  const isActive = router.asPath === href;
+
+  return (
+    <NextLink
+      href={href}
+      className={cn(
+        isActive
+          ? "font-semibold text-gray-800 dark:text-gray-200"
+          : "font-normal text-gray-600 dark:text-gray-400",
+        "hidden md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all"
+      )}
+    >
+      <span className="capsize">{text}</span>
+    </NextLink>
+  );
+}
+
+export default function Container(props: any) {
+  const { children, ...customMeta } = props;
+  const router = useRouter();
+  const meta = {
+    title: "Emanuele Gurini – Developer, writer, creator.",
+    description: `Front-end and software developer, Creator`,
+    image: "",
+    type: "website",
+    ...customMeta,
+  };
+
+  return (
+    <div className="bg-gray-50 dark:bg-gray-900">
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta property="og:url" content={`https://leerob.io${router.asPath}`} />
+        <link rel="canonical" href={`https://leerob.io${router.asPath}`} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Emanuele Gurini" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@EmanueleGurini" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+        {meta.date && (
+          <meta property="article:published_time" content={meta.date} />
+        )}
+      </Head>
+      <div className="flex flex-col justify-center px-8">
+        <nav className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
+          <div className="ml-[-0.60rem]">
+            <MobileMenu />
+            <NavItem href="/" text="Home" />
+            <NavItem href="/blog" text="Blog" />
+          </div>
+        </nav>
+      </div>
+      <main
+        id="skip"
+        className="flex flex-col justify-center px-8 bg-gray-50 dark:bg-gray-900"
+      >
+        {children}
+        <Footer />
+      </main>
+    </div>
+  );
+}
