6dcf3e0bfcb6bfb872a753a24fedff6febcee8c4
emanuelegurini <emanuele.gurini89@gmail.com>
2023-01-05 01:06:15 +0100

    Fix: test-container

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,48 +1,18 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import NextLink from "next/link";
-import cn from "classnames";
-
 import Footer from "./Footer";
-import MobileMenu from "./MobileMenu";
-//import MobileMenu from "components/MobileMenu";
-
-function NavItem({ href, text }: any) {
-  const router = useRouter();
-  const isActive = router.asPath === href;
-
-  return (
-    <NextLink
-      href={href}
-      className={cn(
-        isActive
-          ? "font-semibold text-gray-800 dark:text-gray-200"
-          : "font-normal text-gray-600 dark:text-gray-400",
-        "hidden md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all"
-      )}
-    >
-      <span className="capsize">{text}</span>
-    </NextLink>
-  );
-}
+import Header from "./Header";
 
 export default function Container(props: any) {
-  const { children, ...customMeta } = props;
+  const { children, title, description, type, image, date } = props;
   const router = useRouter();
-  const meta = {
-    title: "Emanuele Gurini – Developer, writer, creator.",
-    description: `Front-end and software developer, Creator`,
-    image: "/images/cover-emanuele-gurini-min.jpg",
-    type: "website",
-    ...customMeta,
-  };
 
   return (
     <div className="bg-gray-50 dark:bg-gray-900">
       <Head>
-        <title>{meta.title}</title>
+        <title>{title}</title>
         <meta name="robots" content="follow, index" />
-        <meta content={meta.description} name="description" />
+        <meta content={description} name="description" />
         <meta
           property="og:url"
           content={`https://emanuelegurini.blog${router.asPath}`}
@@ -52,30 +22,29 @@ export default function Container(props: any) {
           rel="canonical"
           href={`https://emanuelegurini.blog${router.asPath}`}
         />
-        <meta property="og:type" content={meta.type} />
+        <meta property="og:type" content={type} />
         <meta property="og:site_name" content="Emanuele Gurini" />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
+        <meta property="og:description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta
+          property="og:image"
+          content={
+            "https://www.emanuelegurini.blog/static/images/cover-emanuele-gurini-min.jpg"
+          }
+        />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@EmanueleGurini" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
-        {meta.date && (
-          <meta property="article:published_time" content={meta.date} />
-        )}
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta
+          name="twitter:image"
+          content={
+            "https://www.emanuelegurini.blog/static/images/cover-emanuele-gurini-min.jpg"
+          }
+        />
+        {date && <meta property="article:published_time" content={date} />}
       </Head>
-      <div className="flex flex-col justify-center px-8">
-        <nav className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
-          <div className="ml-[-0.60rem]">
-            <MobileMenu />
-            <NavItem href="/" text="Home" />
-            <NavItem href="/about" text="About" />
-            <NavItem href="/blog" text="Blog" />
-          </div>
-        </nav>
-      </div>
+      <Header />
       <main
         id="skip"
         className="flex flex-col justify-center px-8 bg-gray-50 dark:bg-gray-900"
@@ -86,3 +55,11 @@ export default function Container(props: any) {
     </div>
   );
 }
+
+Container.defaultProps = {
+  title: "Emanuele Gurini – Developer, writer, creator.",
+  description: `Front-end and software developer, Creator`,
+  image:
+    "https://www.emanuelegurini.blog/static/images/cover-emanuele-gurini-min.jpg",
+  type: "website",
+};
